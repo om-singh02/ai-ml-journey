e67a684303c95652ac8268e7f414c645ec2e03d5
Om Singh <oms77420@gmail.com>
2026-04-08 20:04:43 -0400

    Phase0 Week 1 Lesson 1- Variable, Types and Strings

---
+++ phase0/week1/lesson1.py
@@ -0,0 +1,46 @@
+# Block 1
+name= "Om Singh"
+age = 24
+temperature = 36.6
+is_learning = True
+
+print(name, age, temperature, is_learning)
+
+#block 2
+x = 10
+print(type(x))
+
+x = "hello"
+print(type(x))
+
+x = 3.14
+print(type(x))
+
+#block 3
+a = 15
+b = 4
+
+print(a+b) # Addition
+print(a-b) # Subtraction
+print(a*b)
+print(a/b)
+print(a//b) # Floor division
+print(a%b)
+print(a**b) # Exponentiation
+
+#block4
+first = "Machine"
+second = "Learning"
+
+full = first + " " + second
+print(full)
+
+phase = 0
+week = 1
+print(f"You are in Phase {phase}, Week {week}")
+
+sentence = " Hello World "
+print(sentence.strip())
+print(sentence.lower())
+print(sentence.upper())
+print(sentence.replace("World", "Python"))
